59b6a60f5573b23af5750f621947ef276e555f2c
Protullen <138602811+protullen@users.noreply.github.com>
2023-09-03 23:11:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from pyrogram import Client, filters, enums
-from time import sleep
 from os import environ
+from calculate_time import get_data
+import time
 
 bot_token = environ.get('BOT_TOKEN')
 
 counter = Client(    
     name='Time-Counter',
-    api_id=6353248,
-    api_hash='1346f958b9d917f0961f3e935329eeee',
-    bot_token=bot_token
+    api_id=24871620,
+    api_hash='e4195bedc71234a179a3d9ac0cad6401',
+    bot_token= bot_token
 )
 
 EDITING = False
@@ -31,78 +32,32 @@ async def counts(bot, update):
     
 @counter.on_message(filters.command('start_count') & filters.user(admin_id)) 
 async def counts(bot, update):
-    global EDITING
+    global EDITING, CANCEL
     if EDITING:
         return await update.reply('Time Counter Already Started!')
-    EDITING = True 
+    EDITING = True
     await update.reply('Time-Counter Started Successfully!')
-    message = await bot.get_messages(-1001593055888, 732)
-    import re
-    pattern = r"(\w+)\s*:\s*(\d+)"
-    matches = re.findall(pattern, message.text)
-    if matches:
-        time_units = {}
-        for unit, value in matches:
-            unit = unit.rstrip('s')
-            time_units[unit] = int(value)
-        year = time_units.get("Year", 0)
-        month = time_units.get("Month", 0)
-        day = time_units.get("Day", 0)
-        hour = time_units.get("Hour", 0)
-        minute = time_units.get("Minute", 0)
-        seconds = 0
-        global CANCEL  
-        CANCEL = False
+    CANCEL = False
+    try:
+      while True:
+        y, m, d, h, mi = get_data()
+        if CANCEL:
+            break
         try:
-            while True:
-                if CANCEL:
-                    print('Time Counter Successfully Stopped!')
-                    break
-                sleep(0.01)
-                seconds += 1
-                if seconds == 60:
-                    minute += 1
-                    seconds = 0
-                    print(minute)
-                    if minute == 60:
-                        hour += 1
-                        minute = 0
-                        if hour == 24:
-                            day += 1
-                            hour = 0
-                            if day == 30:
-                                month += 1
-                                day = 0
-                                if month == 12:
-                                    year += 1
-                                    month = 0
-                        if year > 1:
-                          Year = 'Years'
-                        else:
-                          Year = 'Year'
-                        if month > 1:
-                          Month = 'Months'
-                        else:
-                          Month = 'Month'
-                        if day > 1:
-                          Day = 'Days'
-                        else:
-                          Day = 'Day'
-                        if hour > 1:
-                          Hour = 'Hours'
-                        else:
-                          Hour = 'Hour'
-                        text = f"<b>DFF UPDATES</b>\n\n{Year}: {year}\n{Month}: {month}\n{Day}: {day}\n{Hour}: {hour}\n"
-                        await bot.edit_message_text(
-                            chat_id = -1001593055888,
-                            text = text,
-                            message_id = 732,
-                            parse_mode = enums.ParseMode.HTML
-                        )
+             text = f"<b>Since 3 September 2023</b>\n\n<b>Total Years:</b> {y}\n<b>Total Months:</b> {m}\n<b>Total Days:</b> {d}\n<b>Total Hours:</b> {h}\n<b>Total Minutes:</b> {mi}"
+             await bot.edit_message_text(
+                   chat_id = -1001951908326,
+                   text = text,
+                   message_id = 3,
+                   parse_mode = enums.ParseMode.HTML
+             )
+             time.sleep(60 * 60 * 24)
         except Exception as e:
             print(str(e))
             await update.reply(str(e))
-            EDITING = False          
-
+            EDITING = False
+    except Exception as e:
+            print(str(e))
+            await update.reply(str(e))            
 print('Bot Started!')     
 counter.run()
